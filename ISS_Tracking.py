b273dcb57ef408ecca9e1e9cd0c4a26f7753c9d3
Ben Gothard <bgothard@gothardenterprises.com>
2020-04-27 11:25:32 -0500

    V2 Adjusted for issue where the coordinates have no set address, and added a salutations plus a link for more info on the ISS itself.

--- ISS_Tracking.py
+++ ISS_Tracking.py
@@ -7,6 +7,15 @@ iss_lat = float(iss_coordinates['latitude'])
 iss_lon = float(iss_coordinates['longitude'])
 geolocator = Nominatim(user_agent="ISS_Tracking.py")
 location = geolocator.reverse(f"{iss_lat}, {iss_lon}")
-print(f"The ISS is currently at {location.address} - {iss_coordinates} to be precise - but won't be there for long!")
+tell_me_good_news = f"🚀 The ISS is currently at {location.address},\n🗺 {iss_coordinates} to be precise,\n⏳ but won't be there for long!"
+tell_me_bad_news = f"🚀 The ISS is currently at a location that has no set address, \n🗺 {iss_coordinates} to be precise,\n⏳ but won't be there for long!"
 
 
+if location.address is None:
+    print(tell_me_bad_news)
+else:
+    print(tell_me_good_news)
+
+print("\n😊 Thanks for using the ISS Tracker!\n👍 Come back to track the International Space Station again soon!")
+
+print("\n📚 To read more about what the ISS does, I'd encourage you to visit https://www.nasa.gov/mission_pages/station/main/index.html")
